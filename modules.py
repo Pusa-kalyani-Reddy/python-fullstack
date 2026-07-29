ec790bbf78e067dfb8aea76f171b6ad8d85d8a16
Pusa-kalyani-Reddy <pusakalyani78@gmail.com>
2026-07-29 14:58:35 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,19 @@
+'''def add(a,b):
+    return a+b
+def mul(a,b):
+    return a*b
+def divide(a, b):
+    return a%b
+def flordivision(a, b):
+    return a//b'''
+    
+from datetime import datetime, date, timedelta
+now = datetime.now()
+print(now.year, now.month, now.day)
+print(now.strftime('%H : %M : %S'))
+today= date.today()
+print(today)
+tomorrow = today + timedelta(days = 3)
+print(tomorrow)
+diff =datetime(2025,1,1) - datetime.now()
+print(diff)
